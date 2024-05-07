fcbb3be5a532fd1ba94f9c80a92fcaaa6a54f1ce
mickoissicko <defualtusermick@gmail.com>
2024-05-07 13:28:08 +0200

    move to src/

---
+++ src/prepare.cpp
@@ -0,0 +1,54 @@
+#include <filesystem>
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <fstream>
+
+// yes, im finally shortening stuff
+
+using namespace std;
+
+namespace Fs = std::filesystem; 
+typedef std::ifstream Read; 
+typedef std::ofstream Write;
+
+void MkCfgs()
+{
+    int Ui;
+
+    if (chdir("backer") != 0)
+    {
+        cerr << "error noob" << '\n';
+        exit(1);
+    }
+
+    Write Cfg("prefs.yml");
+
+    if (!Cfg.is_open())
+    {
+        cerr << "couldn't write file" << '\n';
+        exit(1);
+    }
+
+    Cfg << "# coming soon!" << '\n';
+
+    Cfg.close();
+}
+
+void Prepare()
+{
+    cout << "initialising folders..." << '\n';
+
+    if (!Fs::exists(".mix"))
+        Fs::create_directories(".mix");
+
+    cout << "making program folder..." << '\n';
+
+    if (!Fs::exists("backer"))
+        Fs::create_directories("backer");
+
+    cout << "making config..." << '\n';
+
+    MkCfgs();
+}
+
