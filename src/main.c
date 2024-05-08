de92698e08f5da2196b8262fb7b0360c104b1c83
mickoissicko <defualtusermick@gmail.com>
2024-05-08 19:09:07 +0200

    --run now starts the clock

--- src/main.c
+++ src/main.c
@@ -18,6 +18,9 @@ int main(int argc, char** argv)
     if (argc >= 2 && !strcmp(argv[1], "--save"))
         MakeSave();
 
+    if (argc >= 2 && !strcmp(argv[1], "--run"))
+        Clock();
+
     return 0;
 }
 
