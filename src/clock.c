99b81b6de85c643b4475158ad07088998cbc7dd4
mickoissicko <defualtusermick@gmail.com>
2024-05-08 19:09:34 +0200

    add some stuff

--- src/clock.c
+++ src/clock.c
@@ -1,8 +1,27 @@
 #include "../common/common.h"
+#include "../lib/sleep.h"
 
 #include <stdio.h>
 
 void Clock()
 {
-    // to be implemented
+    int Std = GetStdTime();
+    int Del = GetDelTime();
+    int End = GetEndTime();
+
+    MakeSave();
+
+    int Index;
+
+    while (Index != End)
+    {
+        Hlt(Std);
+        DirectSaver();
+
+        if (Index == Del)
+            RemoveOldest();
+
+        Index++;
+    }
 }
+
