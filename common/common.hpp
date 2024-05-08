774380a737eac6f2bfe1d74771b254a6da779180
mickoissicko <defualtusermick@gmail.com>
2024-05-08 13:33:37 +0200

    add c++ common header for new func defs

---
+++ common/common.hpp
@@ -0,0 +1,8 @@
+#ifndef COMMON_HPP
+    #define COMMON_HPP
+#endif
+
+#include <string>
+void Counter(std::string WorkingPath);
+void RenameFolder(std::string WorkingPath, std::string FolderName);
+
