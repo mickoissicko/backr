8c77edb1380d2344f62358e098f23fc024072227
mickoissicko <defualtusermick@gmail.com>
2024-05-10 13:59:17 +0200

    function to download server.py + index.html

---
+++ src/download.c
@@ -0,0 +1,24 @@
+#include "../common/common.h"
+#include "../common/urls.h"
+
+#include <unistd.h>
+#include <stdio.h>
+
+#define AUTO 0
+
+void DownloadScripts()
+{
+    printf("establishing connection...\n");
+
+    char* Cwd;
+    getcwd(Cwd, AUTO);
+
+    char* Path = CDToBackerFolder();
+    chdir(Path);
+
+    Get("server.py", SERVER_FILE_URL);
+    Get("index.html", SERVER_HTML_FILE_URL);
+
+    chdir(Cwd);
+}
+
