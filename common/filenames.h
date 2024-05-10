16fd8f55ded23bacb0a5d3791128131d43b94e2c
mickoissicko <defualtusermick@gmail.com>
2024-05-10 14:36:51 +0200

    header containing filenames

---
+++ common/filenames.h
@@ -0,0 +1,9 @@
+#ifndef FILENAMES_H
+    #define FILENAMES_H
+#endif
+
+#define PY_SERVER "server.py"
+#define INDEX_HTML "index.html"
+#define INDEX_VER "version.info"
+#define INDEX_NEW_VER "new.version.info"
+
