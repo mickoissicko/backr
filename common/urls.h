96b24681aa4f1fd2dcd45430818f05f56fd9e99d
mickoissicko <defualtusermick@gmail.com>
2024-05-10 14:02:39 +0200

    add version info file

--- common/urls.h
+++ common/urls.h
@@ -8,3 +8,6 @@ const static char SERVER_FILE_URL[] =
 const static char SERVER_HTML_FILE_URL[] =
     "https://raw.githubusercontent.com/mickoissicko/backr/main/src/server/index.html";
 
+const static char SERVER_VERSION_FILE[] =
+    "https://raw.githubusercontent.com/mickoissicko/backr/main/src/server/version.info";
+
