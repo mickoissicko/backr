2f976da27b4af5599859e866e78842b2c4789e72
mickoissicko <defualtusermick@gmail.com>
2024-05-11 10:41:15 +0200

    complete update function

--- src/update.c
+++ src/update.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_SIZ 512
 #define AUTO 0
 
 void Update()
@@ -13,15 +14,49 @@ void Update()
     char* Path = CDToBackerFolder();
     chdir(Path);
 
+    FILE* NewVersion;
+    NewVersion = fopen(INDEX_NEW_VER, "r");
+
+    if (NewVersion == NULL)
+    {
+        Get("new.version.info", SERVER_VERSION_FILE);
+        NewVersion = fopen(INDEX_NEW_VER, "r");
+
+        if (NewVersion == NULL)
+        {
+            perror("failed to read (1)\n");
+            exit(1);
+        }
+    }
+
+    char Info[MAX_SIZ];
+
+    fgets(Info, MAX_SIZ, NewVersion);
+    fclose(NewVersion);
+
     RemoveScripts();
 
     printf("fetching resources...\n");
 
     Get(INDEX_HTML, SERVER_HTML_FILE_URL);
     Get(PY_SERVER, SERVER_FILE_URL);
+
+    printf("updating index...\n");
+
     Get(INDEX_VER, SERVER_VERSION_FILE);
 
-    printf("done!\n");
+    FILE* Version;
+    Version = fopen(INDEX_VER, "w");
+
+    if (Version == NULL)
+    {
+        perror("error reading from file (2)\n");
+        exit(1);
+    }
+
+    fprintf(Version, "%s", Info);
+
+    fclose(Version);
     exit(0);
 }
 
